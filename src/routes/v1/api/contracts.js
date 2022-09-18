eb0e9f3debb1d73f3b94942100817fd7ce8856df
Johalf Farina <johalffarina@gmail.com>
2022-09-18 11:46:57 +0100

    Add contracts docs

--- src/routes/v1/api/contracts.js
+++ src/routes/v1/api/contracts.js
@@ -6,7 +6,64 @@ const router = express.Router()
 
 const attributes = ['id', 'clientId', 'contractorId', 'terms', 'status']
 
-/* GET /contracts */
+/**
+ * @swagger
+ * tags:
+ *   name: Contracts
+ *   description: API to get contracts between and client and a contractor. Contracts have 3 statuses, `new`, `in_progress`, `terminated`. Contracts are considered `active` only when in status `in_progress` Contracts group jobs within them.
+ * components:
+ *   schemas:
+ *     Contract:
+ *       type: object
+ *       properties:
+ *         id:
+ *           type: integer
+ *           description: The contract id.
+ *           example: 1
+ *         clientId:
+ *           type: integer
+ *           description: The foreign key to client.
+ *           example: 1
+ *         contractorId:
+ *           type: integer
+ *           description: The foreign key to contractor.
+ *           example: 1
+ *         terms:
+ *           type: string
+ *           description: The terms of the contract.
+ *           example: Lorem ipsum dolor sit amet
+ *         status:
+ *           type: string
+ *           description: Thes status of the contract. The status are 'pending', 'accepted', 'rejected', 'terminated'.
+ *           example: in_progress
+*/
+
+/**
+ * @swagger
+ * /api/v1/contracts:
+ *   get:
+ *     tags: [Contracts]
+ *     security:
+ *      - basicAuth: []
+ *     summary: Retrieve a list of contracts.
+ *     description: Returns a list of contracts only if it belongs to the profile calling the API.
+ *     parameters:
+ *       - in: header
+ *         name: profile_id
+ *         required: true
+ *         description: Numeric ID of the profile contract to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A list of contracts.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               contracts:
+ *                 $ref: '#/components/schemas/Contract'
+*/
 router.get('/', async (req, res) => {
   const profileId = req.get('profile_id')
 
@@ -21,7 +78,50 @@ router.get('/', async (req, res) => {
   res.status(200).json({ contracts })
 })
 
-/* GET /contracts/:id */
+/**
+ * @swagger
+ * /api/v1/contracts/{id}:
+ *   get:
+ *     tags: [Contracts]
+ *     security:
+ *      - basicAuth: []
+ *     summary: Retrieve a single contract.
+ *     description: Returns the contract only if it belongs to the profile calling the API.
+ *     parameters:
+ *       - in: header
+ *         name: profile_id
+ *         required: true
+ *         description: Numeric ID of the profile contract to retrieve.
+ *         schema:
+ *           type: integer
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         description: Numeric ID of the contract to retrieve.
+ *         schema:
+ *           type: integer
+ *     responses:
+ *       200:
+ *         description: A single contract.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 contract:
+ *                   type: object
+ *                   $ref: '#/components/schemas/Contract'
+ *       404:
+ *         description: When a contract is not found.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               properties:
+ *                 errors:
+ *                   type: array
+ *                   description: The error message.
+ *                   example: ['contract not found']
+*/
 router.get('/:id', async (req, res) => {
   const profileId = req.get('profile_id')
 
@@ -39,6 +139,5 @@ router.get('/:id', async (req, res) => {
     res.status(404).json({ errors: ['contract not found'] })
   }
 })
-
 
 module.exports = router
