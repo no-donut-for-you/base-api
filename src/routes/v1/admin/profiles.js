b526f451dfba0e99e4b5ec1017576abfbe826341
Johalf Farina <johalffarina@gmail.com>
2022-09-18 20:56:42 +0100

    Add profiles docs

--- src/routes/v1/admin/profiles.js
+++ src/routes/v1/admin/profiles.js
@@ -5,6 +5,13 @@ const { Profile, Contract, Job } = require('../../../models')
 
 const router = express.Router()
 
+/**
+* @swagger
+* tags:
+*   name: Admin/Profiles
+*   description: Admin API to get profiles who are best paid or paid the most for jobs.
+*/
+
 const validateDates = (req, res) => {
   const format = 'MM-DD-YYYY'
   const { start, end } = req.query
@@ -25,7 +32,47 @@ const validateDates = (req, res) => {
   return { startDate: startDate.format(format), endDate: endDate.format(format) }
 }
 
-/* GET /best-profession */
+/**
+* @swagger
+* /admin/v1/best-profession:
+*   get:
+*     tags: [Admin/Profiles]
+*     security:
+*      - basicAuth: []
+*     summary: Get the most paid profession in a date range.
+*     description:  Returns the profession that earned the most money (sum of jobs paid) for any contactor that worked in the query time range.
+*     parameters:
+*       - in: query
+*         name: start
+*         required: true
+*         description: The start date to query best paid profession. Date format is MM-DD-YYYY
+*         schema:
+*           type: string
+*       - in: query
+*         name: end
+*         required: true
+*         description: The end date to query best paid profession. Date format is MM-DD-YYYY
+*         schema:
+*           type: string
+*     responses:
+*       200:
+*         description: The bast paid profession.
+*         content:
+*           application/json:
+*             schema:
+*               properties:
+*                 bestPaidProfession:
+*                   type: object
+*                   properties:
+*                     profession:
+*                       type: string
+*                       description: The profession name.
+*                       example: Programmer
+*                     total:
+*                       type: decimal
+*                       description: The total paid
+*                       example: 1399.99
+*/
 router.get('/best-profession', async (req, res) => {
   const { startDate, endDate } = validateDates(req, res)
 
@@ -63,7 +110,66 @@ router.get('/best-profession', async (req, res) => {
   return res.status(200).json({ bestPaidProfession })
 })
 
-/* GET /best-clients */
+/**
+* @swagger
+* /admin/v1/best-clients:
+*   get:
+*     tags: [Admin/Profiles]
+*     security:
+*      - basicAuth: []
+*     summary: Get the clients that paid the most for jobs in a date range.
+*     description:  Returns the clients that paid the most for jobs in the query time period. limit query parameter should be applied, default limit is 2
+*     parameters:
+*       - in: query
+*         name: start
+*         required: true
+*         description: The start date to query clients that paid the most for jobs. Date format is MM-DD-YYYY
+*         schema:
+*           type: string
+*       - in: query
+*         name: end
+*         required: true
+*         description: The end date to query clients that paid the most for jobs. Date format is MM-DD-YYYY
+*         schema:
+*           type: string
+*       - in: query
+*         name: limit
+*         required: false
+*         description: The limit of clients to return. Default limit is 2
+*         schema:
+*           type: integer
+*     responses:
+*       200:
+*         description: The clients that paid the most for jobs in a date range.
+*         content:
+*           application/json:
+*             schema:
+*               properties:
+*                 bestClients:
+*                   type: array
+*                   properties:
+*                     id:
+*                       type: integer
+*                       description: The client id.
+*                       example: 1
+*                     firstName:
+*                       type: string
+*                       description: The client first name.
+*                       example: John
+*                     lastName:
+*                       type: string
+*                       description: The client last name.
+*                       example: Doe
+*                     total:
+*                       type: decimal
+*                       description: The total paid
+*                       example: 1399.99
+*                     limit:
+*                       type: integer
+*                       optional: true
+*                       description: The limit of clients to return. Default is 2
+*                       example: 10
+*/
 router.get('/best-clients', async (req, res) => {
   const { start, end, limit = 2 } = req.query
 
